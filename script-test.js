2817df0110fe7781064e92eedb032683088d3801
Wojciech <63100104+Wojciech-Jezierski@users.noreply.github.com>
2023-07-17 18:32:13 +0100

    Create script-test.js

---
+++ script-test.js
@@ -0,0 +1,63 @@
+function fibonacci(n) {
+  if (n <= 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  let i = 5;
+  while (i * i <= num) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+    i += 6;
+  }
+
+  return true;
+}
+
+const generateRandomArray = (length, min, max) => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  return arr;
+};
+
+const bubbleSort = (arr) => {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+
+const array = generateRandomArray(10, 1, 100);
+console.log('Generated Array:', array);
+console.log('Sorted Array:', bubbleSort(array));
+
+const obj1 = {
+  name: 'John',
+  age: 30,
+  city: 'New York',
+};
+
+const obj2 = {
+  ...obj1,
+  email: 'john@example.com',
+};
+
+console.log('Object 1:', obj1);
+console.log('Object 2:', obj2);
